3d959bd073920d888300ff0f0c3071e72bf3993d
thgtvhe7031 <thangtvhe151307@fpt.edu.vn>
2021-10-05 23:27:36 +0700

    thay MentorSkill vào UserSkill

--- HappyProgramming/src/java/dao/UserSkillDAO.java
+++ HappyProgramming/src/java/dao/UserSkillDAO.java
@@ -8,6 +8,9 @@
  */
 package dao;
 
+import entity.Skill;
+import java.util.ArrayList;
+
 /**
  * This class contains methods to help us manipulate UserSkill objects in the
  * database.
@@ -15,5 +18,30 @@ package dao;
  * @author
  */
 public interface UserSkillDAO {
+    
+    /**
+     * Get all Skill of the Mentor in the database
+     *
+     * @param uId of the mentor
+     * @return list of all skills of the mentor
+     */
+    public ArrayList<Skill> getAll_Skill_Mentor(int uId) throws Exception;
+
+    /**
+     * Get all ID of Skill of the Mentor in the database
+     *
+     * @param uId of the mentor
+     * @return list of all id of all skills of the mentor
+     */
+    public ArrayList<String> getAll_Id_Skill_Mentor(int uId) throws Exception;
+
+    /**
+     * Update new Skills for the Mentor in the database
+     *
+     * @param uId it is an int number
+     * @param skill_ids list of id of new skills
+     * @return 1 if update success, 0 if fail
+     */
+    public int updateMentorSkill(int uId, String[] skill_ids) throws Exception;
     
 }
--- HappyProgramming/src/java/dao/impl/UserSkillDAOImpl.java
+++ HappyProgramming/src/java/dao/impl/UserSkillDAOImpl.java
@@ -11,6 +11,11 @@
 package dao.impl;
 
 import context.DBContext;
+import entity.Skill;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  * This class implements from class interface UserSkillDAOImpl. <br>
@@ -20,5 +25,113 @@ import context.DBContext;
  * @author
  */
 public class UserSkillDAOImpl extends DBContext implements dao.UserSkillDAO{
+    
+    /**
+     * Get all Skill of the Mentor in the database
+     *
+     * @param uid of the mentor
+     * @return list of all skills of the mentor
+     */
+    @Override
+    public ArrayList<Skill> getAll_Skill_Mentor(int uid) throws Exception {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<Skill> skillList = new ArrayList<>();
+        SkillDAOImpl skilldao = new SkillDAOImpl();
+
+        Skill skill = new Skill();
+        String sql = "select * from UserSkill where uId='" + uid + "'";
+        try {
+            conn = getConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                skill = skilldao.getSkillById(rs.getString("sId"));
+                skillList.add(skill);
+            }
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(ps);
+            closeConnection(conn);
+        }
+
+        return skillList;
+    }
+
+    /**
+     * Get all ID of Skill of the Mentor in the database
+     *
+     * @param uId of the mentor
+     * @return list of all id of all skills of the mentor
+     */
+    @Override
+    public ArrayList<String> getAll_Id_Skill_Mentor(int uId) throws Exception {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<String> skill_Id_List = new ArrayList<>();
+        String sql = "select * from UserSkill where uId='" + uId + "'";
+        try {
+            conn = getConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                skill_Id_List.add(rs.getString("sId"));
+            }
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(ps);
+            closeConnection(conn);
+        }
+
+        return skill_Id_List;
+    }
+
+    /**
+     * Update new Skills for the Mentor in the database
+     *
+     * @param uId of the mentor
+     * @param skill_ids list of id of new skills
+     * @return 1 if update success, 0 if fail
+     */
+    @Override
+    public int updateMentorSkill(int uId, String[] skill_ids) throws Exception {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int status = 0;
+        String sql = "delete from UserSkill where uId='" + uId + "'";
+        try {
+
+            //delete all skill of mentor
+            conn = getConnection();
+            ps = conn.prepareStatement(sql);
+            status = ps.executeUpdate();
+            // update with new skill
+            String sql_insert = "insert into UserSkill (uId,sId,usStatus)"
+                    + " values (" + uId + ",?,'done')";
+
+            for (String skill_id : skill_ids) {
+                PreparedStatement ps2 = conn.prepareStatement(sql_insert);
+                ps2.setString(1, skill_id);
+                status = ps2.executeUpdate();
+            }
+
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(ps);
+            closeConnection(conn);
+        }
+        return status;
+    }
     
 }
