c39b2617ce8e8b1cabbd457962ff42c88d2753c5
duongyk <vvduong108@gmail.com>
2021-10-05 23:01:11 +0700

    Update DBContext.java

--- HappyProgramming/src/java/context/DBContext.java
+++ HappyProgramming/src/java/context/DBContext.java
@@ -1,98 +1,61 @@
 /*
- * Copyright (C) 2021, FPT University<br>
- * SWP391 - SE1509 - Group 4<br>
- * Happyprogramming<br>
- *
- * Record of change:<br>
- * DATE          Version    Author           DESCRIPTION<br>
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package context;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 /**
- *
- * This class configurate to connect data from database
  *
  * @author 
  */
 public class DBContext {
+    Connection connection = null;
 
-    /**
-     * DBContext initial
-     */
-    private InitialContext initial;
-    /**
-     * DBContext context
-     */
-    private Context context;
-    /**
-     * DBContext dbName
-     */
-    private String dbName;
-    /**
-     * DBContext serverName
-     */
-    private String serverName;
-    /**
-     * DBContext portNumber
-     */
-    private String portNumber;
-    /**
-     * DBContext image
-     */
-    private String image;
-    /**
-     * DBContext username
-     */
-    private String username;
-    /**
-     * DBContext password
-     */
-    private String password;
-
-    /**
-     * Constructor
-     */
     public DBContext() {
         try {
-            this.initial = new InitialContext();
-            this.context = (Context) initial.lookup("java:comp/env");
-            this.serverName = context.lookup("localhost").toString();
-            this.dbName = context.lookup("PRJ_SWP").toString();
-            this.portNumber = context.lookup("1433").toString();
-            this.username = context.lookup("sa").toString();
-            this.password = context.lookup("sa").toString();
-            this.image = context.lookup("imagePath").toString();
-        } catch (NamingException ex) {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=PRJ_SWP", "sa", "sa");
+            System.out.println("Connect successfully !!");
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    /**
-     * Get connection of your database
-     *
-     * @return connection
-     * @throws ClassNotFoundException
-     * @throws SQLException
-     */
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-
-        //String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
-        String url = "jdbc:sqlserver://localhost:1433;databaseName=PRJ_SWP";
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        return DriverManager.getConnection(url, "sa", "sa");
+    
+    public DBContext(String URL, String username, String pass){
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(URL, username, pass);
+            System.out.println("Connect successfully !!");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-
+    
+    public Connection getConnection(){
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=PRJ_SWP", "sa", "sa");
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return connection;
+    }
+    
+    public ResultSet getData(String sql){
+        ResultSet rs = null;
+        try {
+            Statement stm = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            rs = stm.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+    
     /**
      * When you are done with using your connection, you need close in order to
      * release any other database resource
@@ -131,40 +94,4 @@ public class DBContext {
             rs.close();
         }
     }
-
-    /**
-     * Get path of image store in file context
-     *
-     * @return path of image
-     * @throws Exception
-     */
-    public String getImagePath() throws Exception {
-        return image;
-    }
-
 }
-
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//
-//public class DBContext {
-//
-//    public Connection connection;
-//
-//    public DBContext() {
-//        try {
-//            //Change the username password and url to connect your own database
-//            String username = "sa";
-//            String password = "sa";
-//            String url = "jdbc:sqlserver://localhost:1433;databaseName=PRJ_SWP";
-//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//            connection = DriverManager.getConnection(url, username, password);
-//        } catch (ClassNotFoundException | SQLException ex) {
-//            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
-//
-//}
